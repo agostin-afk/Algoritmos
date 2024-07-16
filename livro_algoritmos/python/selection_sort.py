0c31fc5d66703b5d5013f3bf4fd3ecf63fd3d004
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-07-16 15:36:04 -0300

    Update selection_sort.py

---
+++ livro_algoritmos/python/selection_sort.py
@@ -0,0 +1,12 @@
+
+lista=[7, 9, 3, 1, 0, 4]
+def selection_sort(lista):
+    tamanho_lista = len(lista)
+    for j in range(tamanho_lista-1):
+        min_value = j
+        for i in range(j + 1, tamanho_lista):
+            if lista[min_value] > lista[i]:
+                min_value = i
+        lista[j], lista[min_value] = lista[min_value], lista[j]
+    print(lista)
+selection_sort(lista)
